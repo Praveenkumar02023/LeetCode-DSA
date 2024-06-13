6c4e35825b95acf047352ebf29175d20294f67c5
Praveen kumar <praveenkumar042023@gmail.com>
2024-06-13 16:17:34 +0530

    Time: 3 ms (29.62%), Space: 7.3 MB (32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    int solve(int n , vector<int>& dp){
-        if(n <= 1) return dp[n] =  1;
+// Memoization
+    // int solve(int n , vector<int>& dp){
+    //     if(n <= 1) return dp[n] =  1;
 
-        if(dp[n] != -1) return dp[n];
+    //     if(dp[n] != -1) return dp[n];
 
-        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-    }
+    //     return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    // }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        solve(n,dp);
+        // Tabulation
+
+        dp[0] = 1,dp[1] = 1;
+
+        for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i-2]+dp[i-1];
+        }
 
         return dp[n];    
     }
