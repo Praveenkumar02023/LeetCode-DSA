801045e4c231eb62268197e8f528aa41f15e59f2
Praveen kumar <praveenkumar042023@gmail.com>
2024-06-22 00:53:19 +0530

    Time: 51 ms (23.74%), Space: 46.8 MB (35.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -15,55 +15,31 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        // vector<vector<int>> dp(n,vector<int>(amount + 1 , -1));
+        vector<vector<int>> dp(n,vector<int>(amount + 1 , -1));
         
         // int res =  solve(n-1,amount,coins,dp);
         //tabulation
 
-        // for(int target = 0 ; target <= amount ; target++){
-        //     if(target % coins[0] == 0) dp[0][target] = target/coins[0];
-        //     else dp[0][target] = 1e9;
-        // }
-
-        // for(int i = 1 ; i < n ; i++){
-
-        //     for(int target = 0 ; target <= amount ; target++){
-
-        //             int notPick = 0 + dp[i-1][target];
-        //             int Pick = INT_MAX;
-        //             if(coins[i] <= target) Pick = 1 + dp[i][target-coins[i]];
-
-        //             dp[i][target] = min(Pick,notPick);                
-
-        //     }
-        // }
-
-
-        //space optimised
-        
-        vector<int> prev(amount+1,0),cur(amount+1,0);
-
         for(int target = 0 ; target <= amount ; target++){
-            if(target % coins[0] == 0) prev[target] = target/coins[0];
-            else prev[target] = 1e9;
+            if(target % coins[0] == 0) dp[0][target] = target/coins[0];
+            else dp[0][target] = 1e9;
         }
 
         for(int i = 1 ; i < n ; i++){
 
             for(int target = 0 ; target <= amount ; target++){
 
-                    int notPick = 0 + prev[target];
-                    int Pick = 1e9;
-                    if(coins[i] <= target) Pick = 1 + cur[target-coins[i]];
+                    int notPick = 0 + dp[i-1][target];
+                    int Pick = INT_MAX;
+                    if(coins[i] <= target) Pick = 1 + dp[i][target-coins[i]];
 
-                    cur[target] = min(Pick,notPick);                
+                    dp[i][target] = min(Pick,notPick);                
 
             }
-            prev = cur;
         }
 
-        if(prev[amount] == 1e9) return -1;
+        if(dp[n-1][amount] == 1e9) return -1;
         
-        return prev[amount];
+        return dp[n-1][amount];
     }
 };
