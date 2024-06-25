31ab71585437951ba2d88e88f64858773e418e7e
Praveen kumar <praveenkumar042023@gmail.com>
2024-06-25 15:32:00 +0530

    Time: 17 ms (65.8%), Space: 7.6 MB (96.64%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -15,19 +15,20 @@ public:
     int numDistinct(string s, string t) {
         int mod = 1e9 + 7;
         int n = s.size() , m = t.size(); 
-        vector<vector<long long>> dp(n+1,vector<long long>(m+1,0));
+        vector<int> prev(m+1,0),cur(m+1,0);
 
         // return solve(n,m,s,t,dp);
 
-        for(int i = 0 ; i <= n ; i++) dp[i][0] = 1;
-        for(int i = 1 ; i <= m ; i++) dp[0][i] = 0;
+       cur[0] = prev[0] = 1;
+        
 
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= m ; j++){
-                if(s[i-1] == t[j-1] ) dp[i][j] = (dp[i-1][j-1]%mod + dp[i-1][j]%mod)%mod;
-                else dp[i][j] = dp[i-1][j];
+                if(s[i-1] == t[j-1] ) cur[j] = (prev[j-1]%mod + prev[j]%mod)%mod;
+                else cur[j] = prev[j];
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
