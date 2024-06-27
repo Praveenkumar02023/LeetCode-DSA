487832c122d254e2335171f5daa19248419dcc55
Praveen kumar <praveenkumar042023@gmail.com>
2024-06-27 13:59:35 +0530

    Time: 89 ms (44.13%), Space: 99.7 MB (5.68%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
 
-    int solve(vector<int>& arr, int j,int Maxx,vector<int>&dp){
-        if(j < 0) return 0;
+    // int solve(vector<int>& arr, int j,int Maxx,vector<int>&dp){
+    //     if(j < 0) return 0;
 
-        if(dp[j] != -1) return dp[j];
+    //     if(dp[j] != -1) return dp[j];
 
-        int Profit = 0;
-        if(arr[j] > Maxx) Maxx = arr[j];
-        else{
-            Profit = Maxx-arr[j];
+    //     int Profit = 0;
+    //     if(arr[j] > Maxx) Maxx = arr[j];
+    //     else{
+    //         Profit = Maxx-arr[j];
+    //     }
+
+    //     int possProfit = solve(arr,j-1,Maxx,dp);
+
+    //     return dp[j] = max(possProfit,Profit);
+
+    // }
+    int maxProfit(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> dp(n+1,0);
+        // return solve(prices,n-1,INT_MIN,dp);
+        int Minn = INT_MAX;
+        for(int j = 1 ; j <= n ; j++){
+
+            int Profit = 0;
+           
+            if(arr[j-1] < Minn) Minn = arr[j-1];
+            else{
+                Profit = arr[j-1] - Minn;
+            }
+            int possProfit = dp[j-1];
+
+            dp[j] = max(Profit,possProfit);
         }
 
-        int possProfit = solve(arr,j-1,Maxx,dp);
-
-        return dp[j] = max(possProfit,Profit);
-
-    }
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> dp(n,-1);
-        return solve(prices,n-1,INT_MIN,dp);
+        return dp[n];    
     }
 };
