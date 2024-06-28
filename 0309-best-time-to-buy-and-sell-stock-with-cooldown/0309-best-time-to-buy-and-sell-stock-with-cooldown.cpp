1270a73b0af03a3cc7b7a8cc9fa81fcf81ed03a8
Praveen kumar <praveenkumar042023@gmail.com>
2024-06-28 21:43:59 +0530

    Time: 10 ms (9.98%), Space: 14.3 MB (39.99%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    int solve(int i, vector<int>& arr, bool buy, bool coolDown,
-              vector<vector<vector<int>>>& dp) {
-        if (i == arr.size())
-            return 0;
+    int solve(int i, vector<int>& arr, bool buy, vector<vector<int>> &dp) {
+            if(i >= arr.size()) return 0;
+            if(dp[i][buy] != -1) return dp[i][buy];
 
-        if (dp[i][buy][coolDown] != -1)
-            return dp[i][buy][coolDown];
+            int take = 0;
+            if(buy){
+                take = max((-arr[i] + solve(i+1,arr,false,dp)) , solve(i+1,arr , true,dp));
+            }else{
+                take = max((arr[i] + solve(i+2,arr,true,dp)), solve(i+1,arr,false,dp));
+            }
 
-        int take = 0;
-        if (buy && coolDown) {
-            take = dp[i][buy][coolDown] =
-                       max((-arr[i] + solve(i + 1, arr, false, true, dp)),
-                           solve(i + 1, arr, true, true, dp));
-        } else if (buy == false) {
-            take = dp[i][buy][coolDown] =
-                       max((arr[i] + solve(i + 1, arr, true, false, dp)),
-                           solve(i + 1, arr, false, true, dp));
-        } else if (buy && coolDown == false) {
-            take = dp[i][buy][coolDown] = solve(i + 1, arr, true, true, dp);
-        }
+            return dp[i][buy]  = take;
 
-        return take;
     }
     int maxProfit(vector<int>& arr) {
         int n = arr.size();
-        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(2, vector<int>(2, -1)));
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return solve(0, arr, true,dp);
 
-        return solve(0, arr, true, true, dp);
+
     }
 };
