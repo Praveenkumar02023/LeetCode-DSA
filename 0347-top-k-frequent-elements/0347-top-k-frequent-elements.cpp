3a2cf324318a91d06cf8bad07441abf402850da9
Praveen kumar <praveenkumar042023@gmail.com>
2024-07-13 17:02:06 +0530

    Time: 12 ms (62.18%), Space: 17.4 MB (19.2%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -9,13 +9,17 @@ public:
 
      for(int x : nums) mp[x]++;
 
-     priority_queue<pair<int,int>> pq;
+     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
 
      for(auto it : mp){
         pq.push(make_pair(it.second,it.first));
+
+        if(pq.size() > k){
+            pq.pop();
+        }
      }
 
-     while(k--){
+     while(!pq.empty()){
         res.push_back(pq.top().second);
         pq.pop();
      }
