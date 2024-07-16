0540a34273d059acd6eb6a4bf30e08a2903d6f51
Praveen kumar <praveenkumar042023@gmail.com>
2024-07-16 11:32:06 +0530

    Time: 40 ms (77.85%), Space: 51.1 MB (20.22%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
-    bool solve(int i , vector<int>& nums , int n,vector<int>& dp){
-        if(i == n-1) return true;
-        if(i >= n) return false;
+    // bool solve(int i , vector<int>& nums , int n,vector<int>& dp){
+    //     if(i == n-1) return true;
+    //     if(i >= n) return false;
 
-        if(dp[i] != -1) return dp[i];
+    //     if(dp[i] != -1) return dp[i];
 
-        int step = nums[i];
-
-       
+    //     int step = nums[i];
 
        
-        for (int j = 1; j <= step; ++j) {
-            if (solve(i + j, nums, n,dp)) {
-                return dp[i] = true;
-            }
-        }
 
-        return dp[i] = false;
-    }
+       
+    //     for (int j = 1; j <= step; ++j) {
+    //         if (solve(i + j, nums, n,dp)) {
+    //             return dp[i] = true;
+    //         }
+    //     }
+
+    //     return dp[i] = false;
+    // }
 
     bool canJump(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> dp(n,-1);
+        // vector<int> dp(n,-1);
 
-        return solve(0,nums,n,dp);
+        // return solve(0,nums,n,dp);
+
+        int MaxIdx = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(i > MaxIdx) return false;
+            MaxIdx = max(MaxIdx,i+nums[i]);
+        }
+
+        return true;
     }
 };
