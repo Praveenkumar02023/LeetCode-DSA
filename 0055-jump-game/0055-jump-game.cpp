14bfb173d3a6720d9aaa6700682254e0ef101d5e
Praveen kumar <praveenkumar042023@gmail.com>
2024-07-16 10:59:49 +0530

    Time: 483 ms (12.47%), Space: 54.2 MB (6.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool solve(int i , vector<int>& nums , int n,vector<int>& dp){
+        if(i == n-1) return true;
+        if(i >= n) return false;
+
+        if(dp[i] != -1) return dp[i];
+
+        int step = nums[i];
+
+       
+
+       
+        for (int j = 1; j <= step; ++j) {
+            if (solve(i + j, nums, n,dp)) {
+                return dp[i] = true;
+            }
+        }
+
+        return dp[i] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> dp(n,-1);
+
+        return solve(0,nums,n,dp);
+    }
+};
