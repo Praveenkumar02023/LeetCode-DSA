cf14486461e9dbd37280898af429a56c04823f32
Praveen kumar <praveenkumar042023@gmail.com>
2024-07-27 15:05:32 +0530

    Time: 26 ms (36.31%), Space: 29.5 MB (17.01%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -12,21 +12,49 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        queue<TreeNode*> q;
+        // queue<TreeNode*> q;
 
-        q.push(root);
-        int cnt = 0;
+        // q.push(root);
+        // int cnt = 0;
 
-        if(root == nullptr) return cnt;
-        while(!q.empty()){
+        // if(root == nullptr) return cnt;
+        // while(!q.empty()){
 
-            auto node = q.front();
-            q.pop();
-            cnt++;
-            if(node->left) q.push(node->left);
-            if(node->right) q.push(node->right);
-        }   
+        //     auto node = q.front();
+        //     q.pop();
+        //     cnt++;
+        //     if(node->left) q.push(node->left);
+        //     if(node->right) q.push(node->right);
+        // }   
 
-        return cnt;
+        // return cnt;
+
+        if(root == nullptr) return 0;
+
+        int lh = findLeft(root);
+        int rh = findRight(root);
+
+        if(lh == rh) return (1<<lh)-1;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+
+    int findLeft(TreeNode* root){
+        int h = 0 ;
+        while(root != nullptr){
+            h++;
+            root = root->left;
+        }
+
+        return h;
+    }
+    int findRight(TreeNode* root){
+        int h = 0 ;
+        while(root != nullptr){
+            h++;
+            root = root->right;
+        }
+
+        return h;
     }
 };
