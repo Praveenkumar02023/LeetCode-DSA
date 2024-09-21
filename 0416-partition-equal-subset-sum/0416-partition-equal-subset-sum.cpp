106b6c8218ef8a52575a1e2f072f7b2eaae43f4d
Praveen kumar <praveenkumar042023@gmail.com>
2024-09-21 12:01:55 +0530

    Time: 138 ms (62.89%), Space: 99.5 MB (26.66%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
-
-    bool f(int n , int sum , vector<int>& nums,vector<vector<int>>& t){
-
-        if(n == 0){
-            if(sum == 0) return 1;
-            return 0;
-        }  
-
-        if(t[n][sum] != -1) return t[n][sum];
-
-        int take = 0;
-        if(sum >= nums[n-1]){
-            take = f(n-1,sum-nums[n-1],nums,t);
-        }
-        int notTake = f(n-1,sum,nums,t);
-        return t[n][sum] = (take || notTake);
-    }    
+    
 
     bool canPartition(vector<int>& nums) {
        int n = nums.size();
