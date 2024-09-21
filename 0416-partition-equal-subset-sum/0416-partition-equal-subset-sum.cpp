0ed8743208970eda5be1a581f0bc59c08842fd75
Praveen kumar <praveenkumar042023@gmail.com>
2024-09-21 12:01:37 +0530

    Time: 137 ms (63.8%), Space: 99.7 MB (22.89%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -26,9 +26,23 @@ public:
         }
         if(sum % 2 != 0) return 0;
 
-        vector<vector<int>> t(n+1,vector<int>(sum+1,-1));
+        vector<vector<int>> t(n+1,vector<int>((sum/2)+1,0));
 
-        return f(n,sum/2,nums,t);
+        for(int i = 0 ; i < n+1 ; i++){
 
+            t[i][0] = 1; 
+        }
+
+        for(int i = 1 ; i < n+1 ; i++){
+            for(int j = 1 ; j < (sum/2)+1 ; j++){
+                int take = 0;
+                if(j >= nums[i-1]) take = t[i-1][j-nums[i-1]];
+
+                int notTake = t[i-1][j];
+
+                t[i][j] = (take || notTake);
+            }
+        }
+        return t[n][sum/2];
     }
 };
