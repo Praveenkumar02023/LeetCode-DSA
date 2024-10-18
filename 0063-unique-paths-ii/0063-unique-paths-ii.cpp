d703ab3d5a0221d78ad05657f4100f20119f9533
Praveen kumar <praveenkumar042023@gmail.com>
2024-10-18 15:27:30 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
+    int solve(int i , int j, int m , int n , vector<vector<int>>& dp,vector<vector<int>>& arr){
 
-    int solve(int i , int j ,vector<vector<int>>& grid,vector<vector<int>> &dp ){
-        if(i == 0 && j == 0) return 1;
-
-        if(i < 0 || j < 0 ) return 0;
-
-        if(grid[i][j] == 1) return 0;
-
+        if(i == m-1 && j == n-1) return 1;
+        if(i >= m || j >= n) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        int up = solve(i-1,j,grid,dp);
-        int left = solve(i,j-1,grid,dp);
+        int down = 0;
+        int right = 0;
 
-        return dp[i][j] = up + left;
+        if( i < m-1 && arr[i+1][j] != 1) down = solve(i+1,j,m,n,dp,arr);
+        if(j < n-1 && arr[i][j+1] != 1) right = solve(i,j+1,m,n,dp,arr);
+
+        return dp[i][j] = down + right;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        if(grid[0][0] == 1) return 0;
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                if(i == 0 && j == 0) dp[0][0] = 1;
-                else if(grid[i][j] == 1) dp[i][j] = 0;
-                else{
-                    int up = 0 , left = 0;
-                    
-                    if(i > 0) up = dp[i-1][j];
-                    if(j > 0) left = dp[i][j-1];
 
-                    dp[i][j] = up + left;
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 
-                }
+        int m = obstacleGrid.size() , n = obstacleGrid[0].size();
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
 
+        vector<vector<int>> dp(m+1,vector<int>(n+1 , -1));
 
-            }
-        }
-        return dp[n-1][m-1];
+        return solve(0,0,m,n,dp,obstacleGrid);
     }
 };
