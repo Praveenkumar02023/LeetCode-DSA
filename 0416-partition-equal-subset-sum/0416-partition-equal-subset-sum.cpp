66ed7d10f444626cada35d9561a20168a7c66520
Praveen kumar <praveenkumar042023@gmail.com>
2024-10-19 22:34:08 +0530

    Time: 191 ms (43.46%), Space: 99.7 MB (23.04%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    
+     bool f(int i , int n , vector<int>&arr , int sum, vector<vector<int>>&dp){
+        
+        if(i >= n ){
+            if(sum == 0) return true;
+            return false;
+        }
+        if(dp[i][sum] != -1) return dp[i][sum];
+        
+        bool pick = false;
+        if(sum >= arr[i]) pick = f(i+1,n,arr,sum-arr[i],dp);
+        bool notPick = f(i+1,n,arr,sum,dp);
+        return dp[i][sum] = pick || notPick;
+        
+    }
 
     bool canPartition(vector<int>& nums) {
-       int n = nums.size();
-        int sum = 0;
-        for(auto x : nums){
-            sum += x;
-        }
-        if(sum % 2 != 0) return 0;
+        int n = nums.size();
 
-        vector<vector<int>> t(n+1,vector<int>((sum/2)+1,0));
+        int sum = accumulate(nums.begin() ,nums.end(),0);
 
-        for(int i = 0 ; i < n+1 ; i++){
+        if(sum % 2 != 0) return false;
 
-            t[i][0] = 1; 
-        }
-
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j < (sum/2)+1 ; j++){
-                int take = 0;
-                if(j >= nums[i-1]) take = t[i-1][j-nums[i-1]];
-
-                int notTake = t[i-1][j];
-
-                t[i][j] = (take || notTake);
-            }
-        }
-        return t[n][sum/2];
+        sum = sum/2;
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,-1));
+        return f(0,n,nums,sum,dp);
     }
 };
