c2f685aa8ab53479db153e12df077469b254cde3
Praveen kumar <praveenkumar042023@gmail.com>
2024-10-19 22:08:24 +0530

    Time: 4 ms (98.77%), Space: 15.1 MB (5.14%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,53 +1,26 @@
 class Solution {
-public:
-    int solve(int i , int j , vector<vector<int>>& mat,vector<vector<int>> &dp){
+public: 
+    int f(int i , int j , int n , vector<vector<int>>& mat , vector<vector<int>>&dp){
 
-        if (j < 0 || j >= mat[0].size()) return 1e9;
-        if(i == mat.size()-1) return mat[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
+        if(j < 0 ||j >= n) return 1e9+7;
+        if(i == n-1) return mat[i][j];
+        if(dp[i][j] != -1e9+7) return dp[i][j];
 
-        int down = mat[i][j] + solve(i+1,j,mat,dp);
-        int diagLeft = mat[i][j] + solve(i+1,j-1,mat,dp);
-        int diagRight = mat[i][j] + solve(i+1,j+1,mat,dp);
-
-        return dp[i][j] = min({down,diagLeft,diagRight});
+        return dp[i][j] = mat[i][j] + min({f(i+1,j-1,n,mat,dp),f(i+1,j,n,mat,dp),f(i+1,j+1,n,mat,dp)});
     }
-    int minFallingPathSum(vector<vector<int>>& mat) {
-        // vector<vector<int>> dp(mat.size(),vector<int>(mat[0].size(),-1));
-        // int res = 1e9;
-        // // for(int i = 0 ; i < mat[0].size() ;i++){
-        // //     int minn = solve(0,i,mat,dp);
-        // //     res =  min(res,minn);
-        // // }
 
-        // return res;
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n= matrix.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1e9+7));
 
-        vector<int> prev(mat[0].size(),-1);
-        for(int i = 0 ; i < mat[0].size() ; i++){
-            prev[i] = mat[mat.size()-1][i];
+        int ans = 1e9+7;
+
+        for(int i = 0 ; i < n ; i++){
+
+            ans = min(ans,f(0,i,n,matrix,dp));
+
         }
 
-        for(int i = mat.size()-2 ; i >= 0 ; i--){
-            vector<int> curr(mat[0].size(),0);
-            for(int j = mat[0].size()-1 ; j >= 0 ; j--){
-
-                int up = 1e9,diagRight = 1e9 , diagLeft = 1e9;
-                up = mat[i][j] + prev[j];
-                if(j < mat[0].size()-1) diagRight = mat[i][j] + prev[j+1];
-                if(j > 0) diagLeft = mat[i][j] + prev[j-1];
-
-                curr[j] = min({up,diagLeft,diagRight});
-            }
-
-            prev = curr;
-        }
-        int res = 1e9;
-        for(int i = 0 ; i < mat[0].size() ; i++){
-            res = min(res,prev[i]);
-        }
-
-        return res;
-
-
+        return ans;
     }
 };
