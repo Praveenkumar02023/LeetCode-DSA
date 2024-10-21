01c593319af2856cae46ca240c7868ea3c65c1e4
Praveen kumar <praveenkumar042023@gmail.com>
2024-10-21 14:45:00 +0530

    Time: 877 ms (11.9%), Space: 11 MB (78.22%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    int solve(int i , int sum , vector<int>& nums,vector<vector<int>> & dp){
-        if(i == 0 ){
-            if(nums[0] == 0 && sum == 0) return 2;
-            if(nums[0] == sum || sum == 0) return 1;
+    int f(int i , int sum , int target , int n , vector<int>& nums){
 
+        if(i == n){
+            if(sum == target) return 1;
             return 0;
         }
-        if(dp[i][sum] != -1) return dp[i][sum];
 
-        int notPick = solve(i-1,sum,nums,dp);
-        int Pick = 0;
-        if(nums[i] <= sum) Pick = solve(i-1,sum-nums[i] , nums,dp);
+        
+        int plus = f(i+1,sum + nums[i],target,n,nums);
+        int minus = f(i+1,sum-nums[i],target,n,nums);
 
-        return dp[i][sum] = (notPick + Pick);
-
+        return plus + minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalSum = 0;
-        for(auto num : nums) totalSum += num;
+        int n = nums.size();
 
-        int sum = (totalSum-target)/2;
-        if(((totalSum-target) < 0 ) || (totalSum-target)%2 != 0 || sum < 0) return 0;
-
-        vector<vector<int>> dp(nums.size(),vector<int>(sum + 1 , -1));
-
-        return solve(nums.size()-1,sum,nums,dp);
+        return f(0,0,target,n,nums);    
     }
 };
