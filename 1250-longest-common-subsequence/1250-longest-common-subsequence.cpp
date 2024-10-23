8b74bf479fc30aa20fbea0a30a7762eab51702bd
Praveen kumar <praveenkumar042023@gmail.com>
2024-10-23 23:54:43 +0530

    Time: 47 ms (9.63%), Space: 26.2 MB (20.02%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
+    int f(int n , int m , string &t1 , string& t2,  vector<vector<int>> &dp){
 
-    int solve(int i , int j , string& s1 ,string & s2,vector<vector<int>> &dp){
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i] == s2[j]) return  dp[i][j] = 1 + solve(i-1,j-1,s1,s2,dp);
+        if(n < 0 || m < 0) return 0;
+        if(dp[n][m] != -1) return dp[n][m];
 
-        return dp[i][j] =  0 + max(solve(i-1,j,s1,s2,dp),solve(i,j-1,s1,s2,dp));
+        if(t1[n] == t2[m]) return dp[n][m] = 1 + f(n-1,m-1,t1,t2,dp);
+
+        return dp[n][m] = max(f(n-1,m,t1,t2,dp),f(n,m-1,t1,t2,dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
-
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-
-        return solve(n-1,m-1,text1,text2,dp);
+        int n = text1.size() , m = text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return f(n-1,m-1,text1,text2,dp);
     }
 };
