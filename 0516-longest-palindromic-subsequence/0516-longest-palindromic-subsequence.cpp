3976c8009cd1298b52835ca5af89a36f2381fdb2
Praveen kumar <praveenkumar042023@gmail.com>
2024-11-08 11:59:31 +0530

    Time: 56 ms (73.09%), Space: 8.3 MB (88.63%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -6,21 +6,23 @@ public:
 
         int n = s.size();
 
-        vector<vector<int>> t(n+1,vector<int>(n+1,0));
-
+        vector<int> curRow(n+1,0) , prev(n+1,0);
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= n ; j++){
 
                 if(s[i-1] == temp[j-1]){
-                    t[i][j] = 1 + t[i-1][j-1];
+                    curRow[j] = 1 + prev[j-1];
                 }else{
-                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                    curRow[j] = max(prev[j],curRow[j-1]);
                 }
             }
+            prev = curRow;
         }
 
-       
+    //    for(int i = 0 ; i <= n ; i++){
+    //     cout<<cur
+    //    }
 
-        return t[n][n];
+        return curRow[n];
     }
 };
