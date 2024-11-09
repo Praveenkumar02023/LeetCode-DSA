dc561e86a05254c80a9bb1f13896af61863df47a
Praveen kumar <praveenkumar042023@gmail.com>
2024-11-09 13:52:18 +0530

    Time: 11 ms (62.38%), Space: 11.1 MB (94.9%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
-    int solve(int i ,int j, string& s1 , string & s2 , vector<vector<int>>& dp){
-        if(i == 0 || j == 0) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s1[i-1] == s2[j-1]) return dp[i][j] = 1 + solve(i-1,j-1,s1,s2,dp);
-
-        return dp[i][j] = max(solve(i,j-1,s1,s2,dp),solve(i-1,j,s1,s2,dp));
-    }
     int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
+        int n = word1.size() , m = word2.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        int len = solve(n,m,word1,word2,dp);
+        vector<int> prev(m+1,0) , cur(m+1,0);
 
-        return (n-len) + (m-len);
+        for(int i = 1 ; i <= n ; i++){
+
+            for(int j = 1 ; j <= m ; j++){
+
+                if(word1[i-1] == word2[j-1]){
+                    cur[j] = 1 + prev[j-1];
+                }else{
+                    cur[j] = max(cur[j-1] , prev[j]);
+                }
+            }
+
+            prev = cur;
+
+        }
+
+
+        return (n-cur[m]) + (m-cur[m]);
     }
 };
