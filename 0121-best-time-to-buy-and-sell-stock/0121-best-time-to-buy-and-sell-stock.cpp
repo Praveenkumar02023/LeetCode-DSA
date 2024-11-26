c05f5d830783253ce94ed71e925a408859c545f5
Praveen kumar <praveenkumar042023@gmail.com>
2024-11-26 12:25:11 +0530

    Time: 0 ms (100%), Space: 103.2 MB (5.02%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    int f(int i , int n , vector<int>& nums,vector<int> &dp){
+    int helper(int i , int buy, vector<int>& prices , vector<int>&dp){
 
-        if(i >= n) return 0 ;
-        if(dp[i] != -1) return dp[i];
-        int ans = 0;
-        int minPrice = 1e9+7;
-       for(int j = 0 ; j < i ; j++){
+        if(i == prices.size()) return 0;
+       
 
-            minPrice = min(minPrice , nums[j]);
-            ans = max(ans, nums[j] - minPrice);
-       }
-        //notTake
-        ans = max(ans,f(i+1,n,nums,dp));
-
-        return dp[i] =  ans;
-    }
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-    //    vector<int> dp(n+1,-1);
-    //     return f(0,n,prices,dp);
-        int buy = prices[0];
-        int diff = 0;
-        for(int i = 1 ;  i < n ; i++ ){
-            diff = max(diff,prices[i] - buy);
-            if(prices[i] < buy){
-                buy = prices[i];
-            }
+        if(prices[i] < buy){
+            buy = prices[i];
         }
 
-        return diff;
+        return max(prices[i] - buy , helper(i+1,buy,prices,dp));
+
+    }
+   
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        // int buy = prices[0];
+        // int diff = 0;
+        // for(int i = 1 ;  i < n ; i++ ){
+        //     diff = max(diff,prices[i] - buy);
+        //     if(prices[i] < buy){
+        //         buy = prices[i];
+        //     }
+        // }
+
+        // return diff;
+       
+        vector<int> dp(n+1-1);
+        return helper(1 , prices[0] , prices,dp);
     }
 };
