edecd219d3a24375309c4f3803ce71e43256ae28
Praveen kumar <praveenkumar042023@gmail.com>
2024-12-26 14:59:13 +0530

    Time: 12 ms (62.53%), Space: 26.4 MB (25.06%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int solve(int i,int sum ,vector<int>&nums,int target,unordered_map<string,int>& mem){
+    int solve(int i,int sum ,vector<int>&nums,int target,vector<vector<int>>&mem,int totalSum){
 
         if(i == nums.size()){
 
             if(sum == target)return 1;
             else return 0;
         }
-        string curr = to_string(i) + "_" + to_string(sum);
+        
+        if(mem[i][sum + totalSum] != -1) return mem[i][sum + totalSum];
 
-        if(mem.count(curr)) return mem[curr];
+        int plus = solve(i+1,sum + nums[i],nums, target,mem,totalSum);
 
-        int plus = solve(i+1,sum + nums[i],nums, target,mem);
+        int minus = solve(i+1,sum - nums[i],nums,target,mem,totalSum);
 
-        int minus = solve(i+1,sum - nums[i],nums,target,mem);
-
-        return mem[curr] = plus + minus;
+        return mem[i][sum+totalSum] = plus + minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        unordered_map<string,int> mem;
-        return solve(0,0,nums,target,mem);
+        int sum = accumulate(nums.begin(),nums.end(),0);
+
+        vector<vector<int>> mem(n+1,vector<int>(2*sum + 1 , -1));
+
+        return solve(0,0,nums,target,mem,sum);
     }
 };
