1a4ffc7e8a0c809b9821ac4ee9076c6219387795
Praveen kumar <praveenkumar042023@gmail.com>
2024-12-30 19:23:52 +0530

    Time: 2 ms (98.43%), Space: 13 MB (59.1%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -18,8 +18,23 @@ public:
     }
 
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int>dp(high + 2 , -1);
-        int stringSize = 0;
-        return getNumOfStrings(low,high,zero,one,stringSize,dp);
+        vector<int>dp(high + max(zero,one)+1 , 0);
+        
+        // return getNumOfStrings(low,high,zero,one,stringSize,dp);
+
+        for(int i = high ; i >= 0 ; i--){
+
+            int takeZero = dp[i + zero];
+            int takeOne = dp[i + one]; 
+
+            if(i >= low && i <= high){
+                dp[i] = (1 + takeZero + takeOne)%1000000007;
+            }
+            else{
+                dp[i] = (takeZero + takeOne)%1000000007;   
+            }
+        }
+
+        return dp[0];
     }
 };
