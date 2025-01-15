cfd6a3cac85f466f0ea18c5b9295151693ea5989
Praveen kumar <praveenkumar042023@gmail.com>
2025-01-15 21:16:14 +0530

    Time: 3 ms (5.14%), Space: 8.7 MB (5.14%) - LeetHub

---
+++ 2509-minimize-xor/2509-minimize-xor.cpp
@@ -0,0 +1,84 @@
+
+class Solution {
+public:
+   
+    pair<string, int> intToBinaryStringAndCount(int num) {
+        if (num == 0) return {"0", 0};  
+        string binaryStr = "";
+        int setBitCount = 0;
+        
+        while (num > 0) {
+            if (num % 2 == 1) setBitCount++;
+            binaryStr = (num % 2 == 0 ? "0" : "1") + binaryStr;  
+            num /= 2;
+        }
+        
+        return {binaryStr, setBitCount};
+    }
+
+    long long binaryStringToInt(const std::string &binaryStr) {
+        long long result = 0;
+        for (char ch : binaryStr) {
+            result = result * 2 + (ch - '0'); 
+        }
+        return result;
+    }
+
+    int minimizeXor(int num1, int num2) {
+      
+        auto binAndCnt1 = intToBinaryStringAndCount(num1);
+        auto binAndCnt2 = intToBinaryStringAndCount(num2);
+
+        int setBitIn2 = binAndCnt2.second;
+        int setBitIn1 = binAndCnt1.second;
+
+        if (setBitIn2 == setBitIn1) return num1;
+
+        string ansNumBin = "";
+        string num1Bin = binAndCnt1.first;
+
+        if (setBitIn1 > setBitIn2) {
+            for (auto ch : num1Bin) {
+                if (ch != '0') {
+                    if (setBitIn2 > 0) {
+                        ansNumBin.push_back('1');
+                        setBitIn2--;
+                    } else {
+                        ansNumBin.push_back('0');
+                    }
+                } else {
+                    ansNumBin.push_back('0');
+                }
+            }
+        } else {
+            for (auto ch : num1Bin) {
+                if (ch != '0') {
+                    if (setBitIn2 > 0) {
+                        ansNumBin.push_back('1');
+                        setBitIn2--;
+                    } else {
+                        ansNumBin.push_back('0');
+                    }
+                } else {
+                    ansNumBin.push_back('0');
+                }
+            }
+
+            int k = ansNumBin.size() - 1;
+            while (k >= 0 && setBitIn2 > 0) {
+                if (ansNumBin[k] != '1') {
+                    ansNumBin[k] = '1';
+                    setBitIn2--;
+                }
+                k--;
+            }
+
+            while (setBitIn2-- > 0) {
+                ansNumBin.push_back('1');
+            }
+        }
+
+       
+        return binaryStringToInt(ansNumBin);
+    }
+};
